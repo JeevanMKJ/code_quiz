9d143a72376eb3f5af24c2d790ba14e98ff3bdb8
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2022-12-21 09:43:28 -0500

    added the HTML, CSS and JavaScript to project

---
+++ assets/script/script.js
@@ -0,0 +1,45 @@
+/* 
+META SCRIPT
+
+var startTime = 6000;
+var timeToWin = x
+1. - event listener(start game) 
+time = statTime
+show first question 
+
+2. - event listerner(answer)
+ if (answer) {
+   go to next question
+ }
+ else (!answer) {
+  time = time - x
+  got to next question
+ }
+
+3. if (time = 0) {
+  game over -> "You Loose!"
+} or {
+  finsh last question -> "You are a Winner!"
+} 
+
+-> same my initials and score
+
+
+
+*/
+
+/*
+GRADING REQUIREMENTS
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+*/
